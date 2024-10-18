6fc21dcf32000a3de3ad1440e02249ff13690c45
sagarmagar0709 <sagarmagar0709@gmail.com>
2024-10-18 20:36:00 +0530

    new

--- Desktop/newnew/terraform.tf
+++ Desktop/newnew/terraform.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   count         = 2  # This will create 2 instances
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (replace with a suitable AMI ID for your region)
+  ami           = "ami-02114c987fa96ee6c"  # Amazon Linux 2 AMI (replace with a suitable AMI ID for your region)
   instance_type = "t2.micro"  # Change instance type if needed
 
   tags = {
