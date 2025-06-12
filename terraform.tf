269bdf12006ea6a19c01e00e3a5dfe2d000def01
sagarmagar0709 <sagarmagar0709@gmail.com>
2025-06-12 13:07:37 +0530

    Pushing full infra code

--- terraform.tf
+++ terraform.tf
@@ -6,28 +6,22 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "demo-eks-cluster"
   cluster_version = "1.29"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+
+  # Let the module create a VPC for you
+  create_vpc = true
+
+  vpc_cidr = "10.0.0.0/16"
+  subnet_ids = []         # Not needed if create_vpc = true
+  vpc_id     = null       # Not needed if create_vpc = true
+
+  azs = ["us-east-1a", "us-east-1b"]
 
   eks_managed_node_groups = {
     default = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-
-      instance_types = ["t3.medium"]
+      instance_types   = ["t3.medium"]
     }
   }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  name    = "eks-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 }
